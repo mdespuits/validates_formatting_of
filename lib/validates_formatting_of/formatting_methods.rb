c5f14941a5ecc4759571c4053a4e11f9fd46077d
Matt Bridges <mbridges.91@collegeplus.org>
2011-11-22 09:05:11 -0600

    Breaking functionality into a few different files

---
+++ lib/validates_formatting_of/formatting_methods.rb
@@ -0,0 +1,13 @@
+module ValidatesFormattingOf
+  module FormattingMethods
+
+    def email
+      /^[a-z0-9!#$%&'*+\/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+\/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/i
+    end
+
+    def url
+      /^https?:\/\/([^\s:@]+:[^\s:@]*@)?[-[[:alnum:]]]+(\.[-[[:alnum:]]]+)+\.?(:\d{1,5})?([\/?]\S*)?$/iux
+    end
+
+  end
+end
