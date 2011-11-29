72928c26168e08fa6030b9aa1ddbf74cb00aabaa
Matt Bridges <mbridges.91@gmail.com>
2011-11-28 22:44:17 -0600

    More hash updating for rbx and jruby

--- spec/validates_formatting_of/model_additions_spec.rb
+++ spec/validates_formatting_of/model_additions_spec.rb
@@ -1,11 +1,11 @@
 require 'spec_helper'
 
 class Email < SuperModel::Base
-  validates_formatting_of :email, using: :email
+  validates_formatting_of :email, :using => :email
 end
 
 class Webpage < SuperModel::Base
-  validates_formatting_of :url, using: :url
+  validates_formatting_of :url, :using => :url
 end
 
 
@@ -14,19 +14,19 @@ describe ValidatesFormattingOf::ModelAdditions do
   describe "email" do
 
     it "validates that the email provided is valid" do
-      Email.new(email: "example@example.com").should be_valid
-      Email.new(email: "badexample.com").should_not be_valid
-      Email.new(email: "mbridges.91@gmail.com").should be_valid
-      Email.new(email: "some-random%%%strangely-formatted-email@lots.of.subdomains.com").should be_valid
-      Email.new(email: "this__???{}|__should@be-valid.com").should be_valid
+      Email.new(:email => "example@example.com").should be_valid
+      Email.new(:email => "badexample.com").should_not be_valid
+      Email.new(:email => "mbridges.91@gmail.com").should be_valid
+      Email.new(:email => "some-random%%%strangely-formatted-email@lots.of.subdomains.com").should be_valid
+      Email.new(:email => "this__???{}|__should@be-valid.com").should be_valid
     end
 
   end
   describe "url" do
 
     it "validates that the email provided is valid" do
-      Webpage.new(url: 'http://something.com').should be_valid
-      Webpage.new(url: "something else").should_not be_valid
+      Webpage.new(:url => 'http://something.com').should be_valid
+      Webpage.new(:url => "something else").should_not be_valid
     end
   end
 
