5814d0ff3c1a21353a7c0ce73397e606ff6ac0c1
Matt Bridges <mbridges.91@gmail.com>
2011-11-28 23:51:04 -0600

    Adding spec for US Zip code regex
    Refactoring spec for ModelAdditinos

--- spec/validates_formatting_of/model_additions_spec.rb
+++ spec/validates_formatting_of/model_additions_spec.rb
@@ -1,18 +1,12 @@
 require 'spec_helper'
 
-class Email < SuperModel::Base
-  validates_formatting_of :email, :using => :email
-end
-
-class Webpage < SuperModel::Base
-  validates_formatting_of :url, :using => :url
-end
-
-
 describe ValidatesFormattingOf::ModelAdditions do
 
   describe "email" do
 
+    class Email < SuperModel::Base
+      validates_formatting_of :email, :using => :email
+    end
     it "validates that the email provided is valid" do
       Email.new(:email => "example@example.com").should be_valid
       Email.new(:email => "badexample.com").should_not be_valid
@@ -24,10 +18,28 @@ describe ValidatesFormattingOf::ModelAdditions do
   end
   describe "url" do
 
+    class Webpage < SuperModel::Base
+      validates_formatting_of :url, :using => :url
+    end
     it "validates that the email provided is valid" do
       Webpage.new(:url => 'http://something.com').should be_valid
       Webpage.new(:url => "something else").should_not be_valid
     end
+
+  end
+  describe "us_zip" do
+    
+    class USZip < SuperModel::Base
+      validates_formatting_of :zipcode, :using => :us_zip
+    end
+    it "validates that the email provided is valid" do
+      USZip.new(:zipcode => '92348').should be_valid
+      USZip.new(:zipcode => '23434-2348').should be_valid
+      USZip.new(:zipcode => '234').should_not be_valid
+      USZip.new(:zipcode => '23408234').should_not be_valid
+      USZip.new(:zipcode => 'invalid').should_not be_valid
+    end
+
   end
 
 end
