d40b05b010f68e33e437e31cc48f8f4d4f5ba1fd
Matt Bridges <mbridges.91@gmail.com>
2011-11-29 00:00:57 -0600

    Added several new methods for validations
    Still no tests for credit_card ip_address yet

--- lib/validates_formatting_of/validating_methods.rb
+++ lib/validates_formatting_of/validating_methods.rb
@@ -11,5 +11,34 @@ module ValidatesFormattingOf
       /^https?:\/\/([^\s:@]+:[^\s:@]*@)?[-[[:alnum:]]]+(\.[-[[:alnum:]]]+)+\.?(:\d{1,5})?([\/?]\S*)?$/iux
     end
 
+    # No numbers of symbols. allows "-"
+    def alpha
+      /^([^\d\W]|[-])*$/
+    end
+
+    # Letters, numbers, and spaces
+    def alphanum
+      /^[A-Z0-9\s]+$/i
+    end
+
+    # Visa, Mastercard, Discver, and American Express
+    def credit_card
+      /^((4\d{3})|(5[1-5]\d{2})|(6011))-?\d{4}-?\d{4}-?\d{4}|3[4,7]\d{13}$/
+    end
+
+    # US Zip code. ##### or #####-####
+    def us_zip
+      /^\d{5}(-\d{4})?$/
+    end
+
+    # US Phone numbers. (###) ###-#### | (###)###-#### | ###-###-####
+    def us_phone
+      /^([\(]{1}[0-9]{3}[\)]{1}[ |\-]{0,1}|^[0-9]{3}[\-| ])?[0-9]{3}(\-| ){1}[0-9]{4}$/
+    end
+
+    def ip_address
+      /^(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])$/
+    end
+
   end
 end
--- spec/validates_formatting_of/model_additions_spec.rb
+++ spec/validates_formatting_of/model_additions_spec.rb
@@ -41,5 +41,44 @@ describe ValidatesFormattingOf::ModelAdditions do
     end
 
   end
+  describe "alpha" do
+    
+    class Alpha < SuperModel::Base
+      validates_formatting_of :letters, :using => :alpha
+    end
+    it "validates that the email provided is valid" do
+      Alpha.new(:letters => 'abscdsofjsdpfahdsofkajlsdfaspdhjfads').should be_valid
+      Alpha.new(:letters => 'asdfalskdfjhas-dlfhasdksdfaldhfadsfasdfa').should be_valid
+      Alpha.new(:letters => 'adsufasodfksadjfskjdfha98').should_not be_valid
+      Alpha.new(:letters => 'asdf ausdpf98hasdfo alsdf ja8 sd').should_not be_valid
+    end
 
+  end
+  describe "alphanum" do
+    
+    class Alphanum < SuperModel::Base
+      validates_formatting_of :letters_and_numbers, :using => :alphanum
+    end
+    it "validates that the email provided is valid" do
+      Alphanum.new(:letters_and_numbers => 'numbersandlettersarevalid1234567890').should be_valid
+      Alphanum.new(:letters_and_numbers => 'justletters').should be_valid
+      Alphanum.new(:letters_and_numbers => 'letters and numbers 123 with spaces').should be_valid
+      Alphanum.new(:letters_and_numbers => 'adding ; some special ** chars').should_not be_valid
+    end
+
+  end
+  describe "us_phone" do
+    
+    class USPhone < SuperModel::Base
+      validates_formatting_of :phone_number, :using => :us_phone
+    end
+    it "validates that the email provided is valid" do
+      USPhone.new(:phone_number => '(234) 234-3456').should be_valid
+      USPhone.new(:phone_number => '123 123 3456').should be_valid
+      USPhone.new(:phone_number => '(223)123-2347').should be_valid
+      USPhone.new(:phone_number => '2349870238').should_not be_valid
+      USPhone.new(:phone_number => '12349870238').should_not be_valid
+    end
+
+  end
 end
