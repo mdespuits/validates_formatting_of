26dceb2488fd9557038875a51d9bf15a71e691ba
Matt Bridges <mbridges.91@gmail.com>
2011-11-29 18:08:21 -0600

    Adding tests for IP address and credit card validations

--- spec/validates_formatting_of/model_additions_spec.rb
+++ spec/validates_formatting_of/model_additions_spec.rb
@@ -81,4 +81,49 @@ describe ValidatesFormattingOf::ModelAdditions do
     end
 
   end
+  describe "us_phone" do
+    
+    class IPAddress < SuperModel::Base
+      validates_formatting_of :ip, :using => :ip_address
+    end
+    it "validates that the email provided is valid" do
+      IPAddress.new(:ip => '10.10.10').should_not be_valid
+      IPAddress.new(:ip => '999.10.10.20').should_not be_valid
+      IPAddress.new(:ip => '2222.22.22.22').should_not be_valid
+      IPAddress.new(:ip => '22.2222.22.2').should_not be_valid
+      IPAddress.new(:ip => '127.0.0.1').should be_valid
+      IPAddress.new(:ip => '132.254.111.10').should be_valid
+    end
+
+  end
+  describe "us_phone" do
+    
+    class IPAddress < SuperModel::Base
+      validates_formatting_of :ip, :using => :ip_address
+    end
+    it "validates that the email provided is valid" do
+      IPAddress.new(:ip => '10.10.10').should_not be_valid
+      IPAddress.new(:ip => '999.10.10.20').should_not be_valid
+      IPAddress.new(:ip => '2222.22.22.22').should_not be_valid
+      IPAddress.new(:ip => '22.2222.22.2').should_not be_valid
+      IPAddress.new(:ip => '127.0.0.1').should be_valid
+      IPAddress.new(:ip => '132.254.111.10').should be_valid
+    end
+
+  end  
+  # For clarification, NONE of the following numbers are real credit card numbers.
+  # They only match the pattern. These were randomly made for testing.
+  describe "credit_card" do
+    
+    class Client < SuperModel::Base
+      validates_formatting_of :cc, :using => :credit_card
+    end
+    it "validates that the email provided is valid" do
+      Client.new(:cc => '4264-2879-1230-0000').should be_valid # Visa style
+      Client.new(:cc => '6011-1111-0000-2391').should be_valid # Discover style
+      Client.new(:cc => '5422434400828888').should be_valid # Mastercard style
+      Client.new(:cc => '1233444444444444').should_not be_valid # fake
+    end
+
+  end
 end
