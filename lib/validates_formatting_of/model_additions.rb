d6254c7b80ec4ef74401cc4509ad5f318df48e5a
Erik Michaels-Ober <sferik@gmail.com>
2011-12-14 19:17:09 -0800

    Don't pass allow_blank or allow_nil when they're not passed

--- lib/validates_formatting_of/model_additions.rb
+++ lib/validates_formatting_of/model_additions.rb
@@ -11,19 +11,18 @@ module ValidatesFormattingOf
     # This call will ensure that the user-provided email is a valid email. This way,
     # you will not need to find or write your own regex to validate. All of that
     # logic is contained within `validates_formatting_of`
-    def validates_formatting_of(attribute, opts = {})
-
+    def validates_formatting_of(attribute, opts={})
       regex_for_validation = opts[:regex] || validate_with(opts[:using])
-
-      # Add :allow_nil and :allow_blank options. Both are false by default.
-      allow_nil = opts[:allow_nil] || false
-      allow_blank = opts[:allow_blank] || false
       validation_message = opts[:message] || ValidationMessages.message(opts[:using])
-
-      validates attribute,  :format => {  :with     =>  regex_for_validation,
-                                          :message  =>  validation_message },
-                            :allow_nil => allow_nil,
-                            :allow_blank => allow_blank
+      options = {
+        :format => {
+          :with => regex_for_validation,
+          :message => validation_message,
+        }
+      }
+      options.merge!(:allow_nil => opts[:allow_nil]) unless opts[:allow_nil].nil?
+      options.merge!(:allow_blank => opts[:allow_blank]) unless opts[:allow_blank].nil?
+      validates(attribute, options)
     end
 
   private
