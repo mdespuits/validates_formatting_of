b3b5a356c70ff032b93a3f9db524f7e326b5be66
Matt Bridges <mbridges.91@gmail.com>
2012-07-14 14:16:22 -0500

    moving ValidationAddition into its own file

---
+++ lib/validates_formatting_of/validation_addition.rb
@@ -0,0 +1,29 @@
+module ValidatesFormattingOf
+  module ValidationAddition
+    attr_reader :validations
+
+    def add(name, regex, message = nil)
+      @validations ||= {}
+      @validations[name.to_sym] = Validation.new(name.to_sym, regex, message)
+    end
+
+    def select_method(attribute, opts)
+      method = opts[:using].nil? ? attribute : opts[:using]
+      if !exists? method
+        raise MissingValidation, "The method #{method.to_sym.inspect} is not a built-in validation."
+      end
+      self[method]
+    end
+
+    def [](name)
+      if name.to_sym == :ip_address
+        warn "[DEPRECATION] The :ip_address validation for `valdiates_formatting_of` is DEPRECATED. Please update your model validations to use :ip_address_v4. This method will be removed by version 0.7.0."
+      end
+      @validations[name.to_sym]
+    end
+
+    def exists?(name)
+      !@validations[name.to_sym].nil?
+    end
+  end
+end
---
+++ spec/validates_formatting_of/validation_addition_spec.rb
@@ -0,0 +1,7 @@
+require 'validates_formatting_of/validation_addition'
+
+module ValidatesFormattingOf
+  describe ValidationAddition do
+    pending "Add tests for this module"
+  end
+end
