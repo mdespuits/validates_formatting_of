ab862bf78f48f03b429ee0db4fea6826f8709519
Matt Bridges <mbridges.91@gmail.com>
2012-07-14 19:26:42 -0500

    adding specs for ValidationAddition

--- lib/validates_formatting_of/validation_addition.rb
+++ lib/validates_formatting_of/validation_addition.rb
@@ -7,15 +7,15 @@ module ValidatesFormattingOf
       @validations[name.to_sym] = Validation.new(name.to_sym, regex, message)
     end
 
-    def find(attribute, opts)
+    def find(attribute, opts = {})
       method = opts[:using].nil? ? attribute : opts[:using]
-      if !exists? method
-        raise MissingValidation, "The validation method #{method.to_sym.inspect} has not been defined."
-      end
       self[method]
     end
 
     def [](name)
+      if !exists? name
+        raise MissingValidation, "The validation method #{name.to_sym.inspect} has not been defined."
+      end
       if name.to_sym == :ip_address
         warn "[DEPRECATION] The :ip_address validation for `valdiates_formatting_of` is DEPRECATED. Please update your model validations to use :ip_address_v4. This method will be removed by version 0.7.0."
       end
